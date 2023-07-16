8bc332aeeefd274826771c9f1be54e89ce255d96
Anupama155 <119721073+Anupama155@users.noreply.github.com>
2023-07-16 17:55:37 +0530

    import java.util.Scanner; public class clothing {     public static int findPairs(int arr[],int size)     {         int count=0,pairs=0;         for(int i=0;i<size;i++)         {             if(arr[i]!=-1)             {                 count=1;                 for(int j=i+1;j<size;j++)                 {                     if(arr[i]==arr[j])                     {                         count++;                         arr[j]=-1;                     }                 }                 pairs=pairs+count/2;             }         }         return pairs;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a[],i;         n=sc.nextInt();         a=new int[n];         for(i=0;i<n;i++)         a[i]=sc.nextInt();         System.out.println(findPairs(a,n));     } }

--- CLOTHING_STORE.java
+++ CLOTHING_STORE.java
@@ -1,36 +1,35 @@
 import java.util.Scanner;
-public class ArraysDemo
+public class clothing 
 {
-  public static int findPairs(int arr[],int size)
-  {
-     int count=0,pairs=0;
-	 for(int i=0;i<size;i++)
-	 {
-	    if(arr[i]!=-1)
-		{
-		    count=1;
-		   for(int j=i+1;j<size;j++)
-		   {
-		     if(arr[i]==arr[j])
-			   { count++;
-			      arr[j]=-1;
-			   }
-		   } 
-		   pairs=pairs+count/2;
-		}
-		
-	 }
-	 return pairs;
-  }
- public static void main(String args[])
- {
-     Scanner sc=new Scanner(System.in);
-     int n,x[],i;
-     n=sc.nextInt();
-     x=new int[n];
-     for(i=0;i<n;i++)
-      x[i]=sc.nextInt();
-       
-     System.out.println(findPairs(x,n));	 
- }
+    public static int findPairs(int arr[],int size)
+    {
+        int count=0,pairs=0;
+        for(int i=0;i<size;i++)
+        {
+            if(arr[i]!=-1)
+            {
+                count=1;
+                for(int j=i+1;j<size;j++)
+                {
+                    if(arr[i]==arr[j])
+                    {
+                        count++;
+                        arr[j]=-1;
+                    }
+                }
+                pairs=pairs+count/2;
+            }
+        }
+        return pairs;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a[],i;
+        n=sc.nextInt();
+        a=new int[n];
+        for(i=0;i<n;i++)
+        a[i]=sc.nextInt();
+        System.out.println(findPairs(a,n));
+    }
 }
